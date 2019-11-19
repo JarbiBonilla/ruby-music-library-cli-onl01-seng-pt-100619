c9a4b94016ebec402eb5479a4fcf83a3abbe452c
Jarbi Bonilla <jarbibonilla@gmail.com>
2019-11-19 07:07:24 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,13 @@
+require 'pry'
+
 class Song
-  attr_accessor :name, :artist
+  attr_accessor :name, :artist, :genre
   @@all = []
   
-  def initialize(name, artist=nil)
+  def initialize(name, artist=nil, genre=nil)
     @name = name
-    @artist = artist if artist
-    
+    self.artist = artist if artist
+    self.genre = genre if genre
   end
   
   def save
@@ -25,6 +27,35 @@ class Song
     song.save
     song
   end
-
-
+  
+  def artist=(artist)
+   @artist = artist
+   artist.add_song(self)
+  end
+  
+  def genre=(genre)
+    @genre = genre
+    genre.songs << self unless genre.songs.include?(self)
+  end
+  
+  def self.find_by_name(name)
+    self.all.find {|song| song.name == name}
+  end
+  
+  def self.find_or_create_by_name(name)
+    find_by_name(name) || create(name)
+  end
+  
+  def self.new_from_filename(filename)
+    split_file = filename.split(" - ")
+    artist_name, song_name, genre_name = split_file[0], split_file[1], split_file[2].gsub(".mp3", "")
+    
+    artist = Artist.find_or_create_by_name(artist_name)
+    genre = Genre.find_or_create_by_name(genre_name)
+    Song.new(song_name, artist, genre)
+  end
+  
+  def self.create_from_filename(filename)
+    self.new_from_filename(filename).save
+  end
 end
